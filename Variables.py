53018473f6ce0faf71f5c91051d6354ee173a287
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-03-31 18:41:35 +0100

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,26 +1,3 @@
-
-
-
-
-
-# fav_drink ="Coffee"
-# print(fav_drink)
-
-#today
-
-# breakfast = "english tea"
-# lunch = "toast"
-# dinner = "corn flakes"
-
-# print(f"this morning for breakfast I drank {breakfast}, after that I ate {lunch}, then for tea I had {dinner}.")
-
-
-# #no of days in a year
-# a = 365.25
-# #how many years old I am this year
-# b = 31
-# #have an extra pair of brackets on the outside
-# print ((a*b)-32)
-
-
-
+ a = 365.25  #No of days in a year
+ b = 31  #My age
+ print ((a*b)-32) # Tells how many years I have bee living for.
